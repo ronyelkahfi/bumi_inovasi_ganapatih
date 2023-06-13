a273fa11ca6a63dc83b1248c9edfbe305dd55fde
Fatkhur Rony <ronyelkahfi@gmail.com>
2023-06-13 09:23:02 +0700

    comented unused script

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "./swagger/swagger.json";
-
+import ServerlessHttp from "serverless-http";
 const app = express();
-
-app.get("/", (req, res) => {
+const router = express.Router();
+router.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/.netlify/functions/app", router);
+// ServerlessHttp(app);
 app.listen(80, () => {
   console.log("Server berjalan di port 3000");
 });
